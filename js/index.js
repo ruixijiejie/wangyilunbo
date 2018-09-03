42fadf4112352f73b3a603c6b1bd85e0c4ad4816
SoHyun <1819109014@qq.com>
2018-09-03 14:38:04 +0800

    完成

--- js/index.js
+++ js/index.js
@@ -19,4 +19,95 @@ window.onload = function () {
     // 2. 高亮第一个
     var spans = slider_ctrl.children;
     spans[1].setAttribute("class","slider-ctrl-con current");
+
+    // 3. 图片移动
+    var scrollWidth = slider.clientWidth
+    for (var i = 1; i < imgs.length; i++) {
+        imgs[i].style.left = scrollWidth + "px"
+    }
+
+    // 4.按钮点击事件
+    var iNow = 0;
+    for (var k in spans) {
+       spans[k].onclick = function () {
+
+           if (this.className == "slider-ctrl-prev") {
+
+               animate(imgs[iNow],{left: scrollWidth});
+
+               --iNow < 0 ? iNow = imgs.length-1 : iNow
+
+               imgs[iNow].style.left = -scrollWidth + "px"
+
+               animate(imgs[iNow],{left: 0})
+
+               setSquare()
+
+           } else if (this.className == "slider-ctrl-next") {
+
+              autoplay()
+
+           } else {
+
+               var that = this.innerHTML - 1;
+
+               if (that > iNow) {
+
+                   animate(imgs[iNow],{left: -scrollWidth});
+
+                   imgs[that].style.left = scrollWidth + "px"
+
+               } else if (that < iNow) {
+
+                   animate(imgs[iNow],{left: scrollWidth});
+
+                   imgs[that].style.left = -scrollWidth + "px"
+
+               }
+               iNow = that
+
+               animate(imgs[that],{left: 0})
+
+               setSquare()
+           }
+       }
+    }
+    function setSquare() {
+
+        for (var i = 1; i < spans.length-1; i++) {
+
+            spans[i].className = "slider-ctrl-con"
+
+        }
+        spans[iNow+1].className = "slider-ctrl-con current"
+    }
+    // 5. 添加定时器
+    var timer = null;
+
+    timer = setInterval(autoplay,1000);
+
+   function autoplay() {
+
+       animate(imgs[iNow],{left: -scrollWidth});
+
+       ++iNow > imgs.length-1 ? iNow = 0 : iNow
+
+       imgs[iNow].style.left = scrollWidth + "px"
+
+       animate(imgs[iNow],{left: 0})
+
+       setSquare()
+   }
+
+   // 6. 清除定时器
+    slider.onmouseover = function () {
+
+        clearInterval(timer)
+
+    }
+    slider.onmouseout = function () {
+
+       timer = setInterval(autoplay,1000)
+
+    }
 }
